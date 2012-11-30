a09f1f32b7c3ef131218a1e41e8f18c4e4b3f58a
sebasutp <sebasutp@gmail.com>
2012-11-30 16:21:41 -0500

    Merging manuel and sebas versions

--- judge2/app/models/testcase.rb
+++ judge2/app/models/testcase.rb
@@ -2,7 +2,7 @@
 class Testcase < ActiveRecord::Base
   belongs_to :problem
   attr_accessible :jtype,:infile,:outfile
-  has_attached_file :infile, :path => ":rails_root/protected/correct/basename:id.:extension"
+  has_attached_file :infile, :path => ":rails_root/protected/correct/:basename:id.:extension"
   has_attached_file :outfile, :path => ":rails_root/protected/correct/:basename:id.:extension"
   
 end
--- judge2/db/schema.rb
+++ judge2/db/schema.rb
@@ -68,4 +68,22 @@ ActiveRecord::Schema.define(:version => 20121130200942) do
 
   add_index "testcases", ["problem_id"], :name => "index_testcases_on_problem_id"
 
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
 end
