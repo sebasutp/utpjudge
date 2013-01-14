8783960b5cc56af0a59eb174d3568903de55d3f1
Insilico <in_silico@googlegroups.com>
2013-01-14 16:58:42 -0500

    User model logic

--- judge/app/models/user.rb
+++ judge/app/models/user.rb
@@ -1,7 +1,12 @@
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessible :code, :email, :encrypted_password, :name
+  attr_accessor :password
   has_many :submissions
   has_and_belongs_to_many :roles
+  validates :password, {:confirmation => true, :length=>{:within => 6..50},
+    :presence => true, :if => password_required?}
+  validates :email, {:presence=>true, :format => {:with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i}}
 
   def after_create
     r = Role.find(3)
@@ -12,8 +17,33 @@ class User < ActiveRecord::Base
     rids = role_ids & roles
     return rids.count>0
   end
+
+  def authenticated?(password)
+    return self.hashed_password == encrypt(password)
+  end
+
+  def self.authenticate(email,password)
+    user = User.find_by_email(email)
+    return user if user and user.authenticated?(password)
+  end
   
   def self.roles
     return {:root => [1], :psetter=>[1,2], :g_user => [1,2,3]}
+  end
+
+protected
+
+  def encrypt_new_password
+    if not password.blank?
+      self.hashed_password = encrypt(password)
+    end
+  end
+
+  def password_required?
+    return encrypted_password.blank? || password.present?
+  end
+
+  def encrypt(str)
+    return Digest::SHA256.hexdigest(str)
   end
 end
--- judge/test/functional/exercises_controller_test.rb
+++ judge/test/functional/exercises_controller_test.rb
@@ -4,4 +4,10 @@ class ExercisesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_template :index
+    assert_not_nil assigns(:exercises)
+  end
 end
