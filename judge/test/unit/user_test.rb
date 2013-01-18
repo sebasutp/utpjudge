43bcd88de729ce094e3989449d2d01b95b5b37dd
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2013-01-18 13:55:07 -0500

    code for future test

--- judge/test/unit/user_test.rb
+++ judge/test/unit/user_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  %{test "right path" do
+      u = User.newMA(:name=>"Test user name",:email=>"test@test.in",:password=>"sec123",:code=>123)
+      assert u.save
+      u = User.where(:name => "Test user name")
+      assert u.count == 1
+  end
+  
+  test "no data" do
+      u = User.new
+      assert !u.save
+      for p in [:name,:email,:code,:password]
+        assert u.errors[p].any?
+      end
+  end
+  
+  test "missing data" do
+      u = User.newMA(:code=>123)
+      assert !u.save
+      for p in [:name,:email,:code,:password]
+        assert u.errors[p].any?
+      end
+  end
+  
+  test "wrong format email" do
+      u = User.newMA(:name=>"Test user name",:email=>"test.in",:password=>"sec123",:code=>123)
+      assert !u.save
+      for p in [:name,:email,:code,:password]
+        assert u.errors[p].any?
+      end
+  end
+  %}
 end
